fec03d71ce7b63f85c147792f13bdc2273c482b7
GreatRedOrange <149154413+GreatRedOrange@users.noreply.github.com>
2023-11-09 18:39:04 +0200

    commit

--- src/main/java/org/example/DeleteFromArr.java
+++ src/main/java/org/example/DeleteFromArr.java
@@ -1,9 +1,38 @@
 package main.java.org.example;
 
+import java.util.Arrays;
+
 public class DeleteFromArr {
-    int test_array[] = {0,1,2,2,3,0,4,2};
 
-   private static void removeFromArr(int i,int [] arr){
+    public static void main(String[] args) {
 
-   }
+        int test_array[] = {0, 1, 2, 2, 3, 0, 4, 2};
+
+        System.out.println(Arrays.toString(removeFromArr(2, test_array)));
+
+    }
+
+    private static int countsDoubletacts(int num, int[] arr) {
+        int count = 0;
+
+        for (int j : arr) {
+            if (j == num) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private static int[] removeFromArr(int num, int[] arr) {
+
+        int[] temp = new int[arr.length - countsDoubletacts(num, arr)];
+
+        for (int i = 0, k = 0; i < arr.length; i++) {
+            if (arr[i] != num) {
+                temp[k] = arr[i];
+                k++;
+            }
+        }
+        return temp;
+    }
 }
