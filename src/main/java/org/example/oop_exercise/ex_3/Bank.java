403263337c34b03beaf3d3977dfdae5af7c19274
GreatRedOrange <149154413+GreatRedOrange@users.noreply.github.com>
2023-11-10 19:31:16 +0200

    first commit oop ex 3

---
+++ src/main/java/org/example/oop_exercise/ex_3/Bank.java
@@ -0,0 +1,5 @@
+package main.java.org.example.oop_exercise.ex_3;
+
+public class Bank {
+
+}
---
+++ src/main/java/org/example/oop_exercise/ex_3/BankAccount.java
@@ -0,0 +1,22 @@
+package main.java.org.example.oop_exercise.ex_3;
+
+public class BankAccount {
+    private int accountNumber;
+    private double balance;
+    private String owner;
+
+    BankAccount(int accountNumber, double balance, String owner) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.owner = owner;
+    }
+
+    public void deposit(double amount) {
+
+    }
+
+    public void withdraw(double amount) {
+
+    }
+
+}
