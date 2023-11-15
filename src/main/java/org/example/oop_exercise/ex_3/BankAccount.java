0b13736aa23c2781f1e9610d421957cb85a3252c
GreatRedOrange <149154413+GreatRedOrange@users.noreply.github.com>
2023-11-15 10:38:29 +0200

    second commit oop ex 3

--- src/main/java/org/example/oop_exercise/ex_3/BankAccount.java
+++ src/main/java/org/example/oop_exercise/ex_3/BankAccount.java
@@ -5,18 +5,35 @@ public class BankAccount {
     private double balance;
     private String owner;
 
-    BankAccount(int accountNumber, double balance, String owner) {
+    public BankAccount(int accountNumber, double balance, String owner) {
         this.accountNumber = accountNumber;
         this.balance = balance;
         this.owner = owner;
     }
 
     public void deposit(double amount) {
-
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposit of $" + amount + " successful. New balance: $" + balance);
+        } else {
+            System.out.println("Invalid deposit amount. Amount must be greater than 0.");
+        }
     }
 
     public void withdraw(double amount) {
-
+        if (amount > 0 && balance - amount >= 0) {
+            balance -= amount;
+            System.out.println("Withdrawal of $" + amount + " successful. New balance: $" + balance);
+        } else {
+            System.out.println("Invalid withdrawal amount or insufficient funds.");
+        }
     }
 
+    public double getBalance() {
+        return balance;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
 }
