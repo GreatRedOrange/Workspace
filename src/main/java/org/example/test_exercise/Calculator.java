0ca122dd074f12bd8cab95790d186b4b65d3d267
GreatRedOrange <149154413+GreatRedOrange@users.noreply.github.com>
2023-11-27 16:21:15 +0200

    first commit - add class Calculator and tests

---
+++ src/main/java/org/example/test_exercise/Calculator.java
@@ -0,0 +1,11 @@
+package org.example.test_exercise;
+
+public class Calculator {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public boolean evenOrOdd(int number) {
+        return number % 2 == 0;
+    }
+}
