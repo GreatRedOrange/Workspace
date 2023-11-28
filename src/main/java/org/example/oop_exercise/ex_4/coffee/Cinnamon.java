bd34d582b5270cf2cf78eee3a5524eee5234cfa8
GreatRedOrange <149154413+GreatRedOrange@users.noreply.github.com>
2023-11-28 15:11:23 +0200

    second commit - changed classes Student and StudentBehaviour. added classes CoffeeMachine, Milk, Sugar, Cinnamon Ex 4 oop

---
+++ src/main/java/org/example/oop_exercise/ex_4/coffee/Cinnamon.java
@@ -0,0 +1,10 @@
+package org.example.oop_exercise.ex_4.coffee;
+
+public class Cinnamon implements Topping {
+    String topping = "cinnamon";
+
+    @Override
+    public String add() {
+        return topping;
+    }
+}
